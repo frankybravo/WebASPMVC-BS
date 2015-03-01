f61ccd4ce0bf67ded5062beed24a9a7842d3d5ab
frank lynch <franklynch67@gmail.com>
2015-03-01 15:54:00 +0000

    add test

--- WebASPMVC_BS.Tests/Controllers/HomeControllerTest.cs
+++ WebASPMVC_BS.Tests/Controllers/HomeControllerTest.cs
@@ -22,8 +22,10 @@ namespace WebASPMVC_BS.Tests.Controllers
             // Act
             ViewResult result = controller.Index() as ViewResult;
 
+            // Assetr
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.IsNotNull(result);
+            //Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
         }
 
         [TestMethod]
