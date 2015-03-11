9e16986f8c55a014a97d1893701101a348a760d8
Frank Lynch <franklynch67@gmail.com>
2015-03-11 18:04:15 +0000

    Revert "layout mess around"
    This reverts commit f56f4e4a5ba95b9cd324a07d4a4477c99c763869.

--- WebASPMVC-BS/App_Data/App_Start/BundleConfig.cs
+++ WebASPMVC-BS/App_Data/App_Start/BundleConfig.cs
@@ -8,15 +8,13 @@ namespace WebASPMVC_BS
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-            
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                         "~/Scripts/bootstrap.js"));
+
+            
             
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
@@ -29,10 +27,13 @@ namespace WebASPMVC_BS
                         */
                         ));
             
-          
-            //styles
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
-            /*
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css"));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
@@ -46,7 +47,6 @@ namespace WebASPMVC_BS
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
-        */
-             }
+        }
     }
 }
